8b4c8b75dee1aee3a746b350fbfd2d8d737f681b
YasminAzar <44767252+YasminAzar@users.noreply.github.com>
2018-12-16 17:35:32 +0200

    Add files via upload
    Adds a "Map" class that displays the map image and converts coordinates to pixels. In addition to this class there is a "Map Scale" class that changes the size of the image according to the screen size

--- Map.java
+++ Map.java
@@ -1,36 +1,65 @@
 package GIS;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 import Coords.MyCoords;
-import GUI.My_GUI;
 import Geom.Point3D;
 
 public class Map {
-	
-//	private JPanel _panel;
-	
-//	public Map(){
-//		Point3D p2 = null;
-//		Point3D p1 = null;
-//		map(p1, p2);
-//	}
-	public static void  map (Point3D p1, Point3D p2) {
 
-	My_GUI frame= new My_GUI();
-	frame.setBounds(0, 0, frame.myImage.getWidth(), frame.myImage.getHeight());
-	frame.createGui();
-	frame.setVisible(true);
-	MyCoords coo= new MyCoords();
-//	Point3D p=new Point3D();
-	coo.azimuth_elevation_dist(p1, p2);
-		
-}
-	
-	public static void main(String[] args) {
-		
-		Point3D gps0=new Point3D(32.103315,35.209039,670.0);
-		Point3D gps1=new Point3D(32.106352 ,35.205808194261195 ,650.0);
-		map(gps0, gps1);
-		System.out.println();
-		
+	static MyCoords coo= new MyCoords();
+
+	public static void mapFrame() {
+		try {
+			Image img = null;
+			img = ImageIO.read(new File("Ariel1.png"));
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLayout(new BorderLayout());
+			frame.add(new MapScale(img));
+			frame.pack();
+			frame.setVisible(true);
+		} catch (Exception exp) {
+			exp.printStackTrace();
+		}
 	}
+
+	public static void  angleBetPoints (Point3D p1, Point3D p2) {
+		float R=6371000; //Radios of earth in meter
+		double Lon_Norm=Math.cos(Math.toRadians(p1.x()));
+		double disLat=(p2.x()*Math.PI/180)-(p1.x()*Math.PI/180);
+		double disLon=(p2.y()*Math.PI/180)-(p1.y()*Math.PI/180);
+		//	System.out.println("The disLat is: "+disLat);
+		//	System.out.println("The disLon is: "+disLon);
+		double disLat_in_meter=Math.sin(disLat)*R;
+		double disLon_in_meter=Math.sin(disLon)*R*Lon_Norm;
+		double azimuth=0;
+		azimuth=Math.atan(disLon_in_meter/disLat_in_meter);
+		azimuth=Math.toDegrees(azimuth);
+		if(disLat_in_meter<0) azimuth=180-azimuth;
+		if(disLat_in_meter<0 && disLon_in_meter<0) azimuth=180+azimuth;
+		if(disLon_in_meter<0) azimuth=360+azimuth;
+		System.out.println("The angel between the two points is: "+azimuth);
+	}
+
+	public static void  disBetPoints (Point3D p1, Point3D p2) {
+		double dxy=coo.distance3d(p1,p2)*coo.distance3d(p1,p2);
+		double dz=p2.z()-p1.z();
+		double dist=Math.sqrt(dxy*dxy+dz*dz);
+		System.out.println("The distance in meterã bwtween the two points is: "+dist);
+	}
+
+
+	public static void main(String[] args) {
+		Point3D p1=new Point3D(32.103315,35.209039,670.0);
+		Point3D p2=new Point3D(32.106352 ,35.205225 ,650.0);
+		Map.mapFrame();
+		Map.angleBetPoints(p1, p2);
+		Map.disBetPoints(p1, p2);
+
+	}
+
 }
---
+++ MapScale.java
@@ -0,0 +1,49 @@
+package GIS;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+	class MapScale extends JPanel{
+		
+	  private static final long serialVersionUID = 1L;
+	  
+	  private Image myImage;
+	  private Image scaled;
+
+	  public MapScale(String myImage) {
+	    this(new ImageIcon(myImage).getImage());
+	  }
+
+	  public MapScale(Image myImage) {
+	    this.myImage = myImage;
+	  }
+
+	  @Override
+	  public void invalidate() {
+	    super.invalidate();
+	    int width = getWidth();
+	    int height = getHeight();
+
+	    if (width > 0 && height > 0) {
+	      scaled = myImage.getScaledInstance(getWidth(), getHeight(),Image.SCALE_SMOOTH);
+	    }
+	  }
+
+	  @Override
+	  public Dimension getPreferredSize() {
+	    return myImage == null ? new Dimension(200, 200) : new Dimension(
+	    		myImage.getWidth(this), myImage.getHeight(this));
+	  }
+
+	  @Override
+	  public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.drawImage(scaled, 0, 0, null);
+	  }
+	  
+	}
+
